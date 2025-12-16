e8efc5d35b80d67626af5258c63727e99d75795c
rukshinia <rukshinia69@gmail.com>
2025-12-16 01:52:05 -0800

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float x1,x2,y1,y2,distance;
+printf("Enter x1 and y1 (coordinates of the first point):");
+scanf("%f%f",&x1,&y1);
+printf("Enter x2 and y2 (coordinates of the second point):");
+scanf("%f%f",&x2,&y2);
+printf("The straight line distance between the two points if: %.2f \n",distance);
+}
